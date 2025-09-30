eb414a4509675eb7ceb8578736fd8dff41f8650e
Monthon Sova <igaccmonthon1150@gmail.com>
2025-09-30 22:06:04 +0700

    Create lab101.c

---
+++ lab101/lab101.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main( void ) { 
+    int a , b , c ; 
+    int sum ; 
+    scanf( "%d %d %d" , &a , &b , &c ) ; 
+    sum = a + b + c ; 
+    printf( "Answer = %d\n" , sum ) ; 
+    return 0 ; 
+}
